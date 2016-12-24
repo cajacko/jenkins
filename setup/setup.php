411cc5056890c0009f2e5c0e09dfd0eb5438c843
Charlie Jackson <charlie@charliejackson.com>
2016-12-24 09:37:11 +0000

    Temp: Started setting up scripts to create ssh keys and copy the public key to digital ocean

---
+++ setup/setup.php
@@ -0,0 +1,74 @@
+<?php
+
+exec('ssh-keygen -t rsa', $output, $status);
+file_get_contents('~/.ssh/id_rsa.pub');
+
+
+// require __DIR__ . './vendor/autoload.php';
+//
+// use DigitalOceanV2\Adapter\BuzzAdapter;
+// use DigitalOceanV2\DigitalOceanV2;
+//
+// // create an adapter with your access token which can be
+// // generated at https://cloud.digitalocean.com/settings/applications
+// $adapter = new BuzzAdapter($_SERVER['DIGITAL_OCEAN_API_KEY']);
+//
+// // create a digital ocean object with the previous adapter
+// $digitalocean = new DigitalOceanV2($adapter);
+//
+// $key = $digitalocean->key();
+// $keys = $key->getAll();
+// $ssh_ids = array();
+//
+// foreach ($keys as $key) {
+//   if (strpos($key->name, 'jenkins') !== false) {
+//     $ssh_ids[] = $key->id;
+//   }
+// }
+//
+// if (!count($ssh_ids)) {
+//   throw new Exception('Could not get ssh key id');
+// }
+//
+// // return the account api
+// $droplet = $digitalocean->droplet();
+//
+// $created = $droplet->create('charliejackson-dev', 'lon1', '512mb', 'docker', false, false, false, $ssh_ids);
+//
+// $id = $created->id;
+//
+// function doesIpExist($droplet, $id) {
+//     $new_droplet = $droplet->getById($id);
+//
+//     if (!$new_droplet->networks) {
+//         return false;
+//     }
+//
+//     if (!($new_droplet->networks[0])) {
+//         return false;
+//     }
+//
+//     if (!($new_droplet->networks[0]->ipAddress)) {
+//         return false;
+//     }
+//
+//     return $new_droplet->networks[0]->ipAddress;
+// }
+//
+// for ($i = 0; $i < 10; $i++) {
+//     if ($i != 0) {
+//         sleep(1);
+//     }
+//
+//     $ip = doesIpExist($droplet, $id);
+//
+//     if ($ip) {
+//         break;
+//     }
+// }
+//
+// if (!$ip) {
+//     throw new Exception('Could not get IP');
+// }
+//
+// echo $ip;
