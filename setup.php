9e67afc80aef9463904c8a9dea8bd2e36f545d6e
Charlie Jackson <charlie@charliejackson.com>
2016-12-30 15:38:38 +0000

    Removed unnecessary logs

--- setup.php
+++ setup.php
@@ -3,16 +3,12 @@
 
 $ssh_key = file_get_contents('/var/lib/jenkins/.ssh/id_rsa.pub');
 
-var_dump($ssh_key);
-
 require __DIR__ . '/vendor/autoload.php';
 
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 use DigitalOceanV2\Adapter\BuzzAdapter;
 use DigitalOceanV2\DigitalOceanV2;
-
-print_r($_SERVER);
 
 // create an adapter with your access token which can be
 // generated at https://cloud.digitalocean.com/settings/applications
@@ -46,9 +42,5 @@ while ($continue_while) {
   $id++;
 }
 
-var_dump($name);
-
 // return the created Key entity
 $createdKey = $key->create($name, $ssh_key);
-
-var_dump($createdKey);
